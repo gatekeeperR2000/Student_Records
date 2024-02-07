81c0a7621230020b462dc9c12311fccd45774f3d
gatekeeperR2000 <122000590+gatekeeperR2000@users.noreply.github.com>
2024-02-06 18:40:53 -0700

    Added basic menu functionality

--- Student_Records.py
+++ Student_Records.py
@@ -10,9 +10,40 @@ cursor = connection.cursor()
 # Create a table students, to hold ID, name, and GPA
 cursor.execute('''
                CREATE TABLE IF NOT EXISTS students (
-               ID INTEGER PRIMARY KEY,
+               id INTEGER PRIMARY KEY,
                name TEXT NOT NULL,
                gpa FLOAT
                )
                ''')
 connection.commit()
+
+# def add_student()
+
+# def get_student()
+    
+# def update_gpa()
+
+# def get_gpa()
+
+# def generate_report()
+    
+# def display_menu()
+
+done = False
+while(not done):
+    # display_menu()
+    choice = int(input("Enter your menu choice: "))
+    match choice:
+        case (1):
+            # Add a Student
+            print(1)
+        case (2):
+            # update a students GPA
+            print(2)
+        case (0):
+            # Exit Program
+            done = True
+        case _:
+            print("Error")
+
+
